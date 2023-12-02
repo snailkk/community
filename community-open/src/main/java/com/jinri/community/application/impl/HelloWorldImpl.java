67154b4211fb3b2c208c2e43108dcffb370c5ddd
snailkk <kangwsh22@163.com>
2023-12-02 10:00:43 +0800

    测试

--- community-open/src/main/java/com/jinri/community/application/impl/HelloWorldImpl.java
+++ community-open/src/main/java/com/jinri/community/application/impl/HelloWorldImpl.java
@@ -9,6 +9,6 @@ public class HelloWorldImpl implements HelloWorld {
 
     @GetMapping("/test")
     public String helloWorld(){
-        return "zheshishenme!";
+        return "welcome to snailkk's world!";
     }
 }
